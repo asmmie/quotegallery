3d4a8018e85cfca65db31f93085aebe057c60508
asmmie <110878912+asmmie@users.noreply.github.com>
2022-10-07 16:09:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 let quote = document.getElementById("quote");
 let author = document.getElementById("author");
-const fade = document.querySelector(".fade-in");
+const fades = document.querySelectorAll(".fade");
 
 const url = "https://api.quotable.io/random";
+
+const fadeIn = () => {
+  [...fades].forEach((fade) => {
+    fade.style.opacity = `1`;
+    fade.style.transition = `all 2.5s ease`;
+  });
+};
+
+const fadeOut = () => {
+  [...fades].forEach((fade) => {
+    fade.style.opacity = `0`;
+    fade.style.transition = `all 2.5s ease`;
+  });
+};
 
 let getQuote = () => {
     fetch(url).then((data) => data.json()).then((item) => {
         quote.innerText = item.content;
         author.innerText = item.author;
+        fadeIn();
+        setTimeout(fadeOut, 7500);
     });
 };
 window.addEventListener('load', getQuote);
